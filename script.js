5f69da6e1ee49cdaa27325351db6cac42ee094f3
mattlundin <74123405+mattlundin@users.noreply.github.com>
2021-11-08 19:30:16 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const images = document.querySelector('.image-container');
+const img = document.querySelectorAll('img');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+
+let idx = 0;
+let interval = setInterval(run, 3000);
+
+function run() {
+	idx++;
+	changeImage();
+}
+
+function changeImage() {
+	if (idx > img.length - 1) {
+		idx = 0;
+	} else if (idx < 0) {
+		idx = img.length - 1;
+	}
+	images.style.transform = `translateX(${-idx * 450}px)`;
+}
+
+function resetInterval() {
+	clearInterval(interval);
+	interval = setInterval(run, 3000);
+}
+
+nextBtn.addEventListener('click', () => {
+	idx++;
+
+	changeImage();
+	resetInterval();
+});
+
+prevBtn.addEventListener('click', () => {
+	idx--;
+
+	changeImage();
+	resetInterval();
+});
